50fb49f4b5c4f2ba8eb27542e6347c4a3ce1c1e4
aaronMan12 <aaronhernandez597@gmail.com>
2022-12-06 16:08:11 -0600

    conexión exitosa

--- src/main/java/mx/uv/Conexion.java
+++ src/main/java/mx/uv/Conexion.java
@@ -3,20 +3,21 @@ package mx.uv;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
 
 public class Conexion {
-    private static String driverName = "com.mysql.jdbc.Driver";
-    private static String url = "mysql://root:rgsd9O8LdB5YZZVqHaah@containers-us-west-59.railway.app:7300/railway";
+    private static String driverName = "com.mysql.cj.jdbc.Driver";// com.mysql.cj.jdbc.Drivercom.mysql.jdbc.Driver
+    private static String url = "jdbc:mysql://containers-us-west-59.railway.app:7300/railway";
     private static String username = "root";
-    private static String password = "rgsd9O8LdB5YZZVqHaah";
+    private static String password = "uj27wZj76GWkvqlHTVfd";
+    private static String aceeso = (url + username + password);
     // variable de conexion
     private static Connection connection = null;
 
     public static Connection getConnection() {
         try {
             Class.forName(driverName);
-            connection = (Connection) DriverManager.getConnection(url, username, password);
+            connection = (Connection) DriverManager.getConnection(aceeso);
 
         } catch (SQLException e) {
             System.out.println(" SQL:" + e);
