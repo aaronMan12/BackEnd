bea70b6efecd78a7c0259445cf4600a610283349
Joyce <jegalo2001@gmail.com>
2022-12-06 23:00:29 -0600

    actualizacion

--- src/main/java/mx/uv/App.java
+++ src/main/java/mx/uv/App.java
@@ -3,10 +3,33 @@ package mx.uv;
 import static spark.Spark.*;
 
 public class App {
-  public static void main(String[] args) {
+  /**
+ * @param args
+ */
+public static void main(String[] args) {
     port(DaPuerto());
     get("/hola", (req, res) -> ("Hello Heroku"));
     Conexion.getConnection();
+  
+    options("/*", (request, response) -> {
+            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
+            System.out.println(accessControlRequestHeaders);
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
+            }
+            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
+            System.out.println(accessControlRequestMethod); 
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
+            }
+            return "OK";
+        });
+
+
+        before((req, res)-> res.header("Access-Control-Allow-Origin", "*"));
+  
+  
+  
   }
 
   private static int DaPuerto() {
